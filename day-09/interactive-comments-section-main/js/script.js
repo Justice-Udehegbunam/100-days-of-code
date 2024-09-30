c8e61b0dab72bdbb99c17739b5c5b9cd8e569991
Justice Udehegbunam <140954375+Justice-Udehegbunam@users.noreply.github.com>
2024-09-30 20:03:30 +0100

    Functionalities needed by the app

---
+++ day-09/interactive-comments-section-main/js/script.js
@@ -0,0 +1,11 @@
+console.log("Hello world");
+
+/* TASKS
+1. Be able to retrieve messages from data.json
+2. be able to send messages that will be saved in data.json
+3. Be able to have a button that switches between users.
+4. Be able to store the actual time that a message was posted 
+5. Be able to edit or delete a particular message that was written by the user 
+6. Reply to Specific Comments
+7. Like or Upvote/Downvote Messages
+8. */
---
+++ day-09/readme.md
@@ -0,0 +1,3 @@
+# Day-09-Till Day-11
+
+Honnestly though i feel i should be done alot sooner but that is currently my target to see how i can finish the base functionality of this project by day 11
