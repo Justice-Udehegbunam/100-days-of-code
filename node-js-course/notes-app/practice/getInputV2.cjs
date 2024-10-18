be3ebe1594efb5d7bc736fc38dfac1917a967c0e
Justice Udehegbunam <justicefavour491@gmail.com>
2024-10-18 23:20:37 +0100

    Day 26

--- node-js-course/notes-app/practice/getInputV2.cjs
+++ node-js-course/notes-app/practice/getInputV2.cjs
@@ -1,17 +1,35 @@
 // Using Yargs
 const yargs = require("yargs");
 
-console.log(yargs.version("1.2.3"));
+//console.log(process.argv);
 
+// version
+/* To find the verion that yargs starts you on you have to check it by "node <appName>.cjs --version/--help if u need help a bit"  
+But yargs usually starts up with version 1.0.0 */
+
+//Customizing yargs version
+yargs.version("1.2.3"); // prints 1.2.3 to the console
 // create add command
 
 yargs.command({
-  command: "name",
-  desc: "my name",
+  command: "add",
+  desc: "Add command for notes application",
   handler: function () {
     console.log("Adding a new notes");
   },
 });
 
+// this creates a command with the name add that basically executes the handler function whenever the add command is called
+
+yargs.command({
+  command: "remove",
+  desc: "remove note",
+  handler: () => {
+    console.log("Removing notes from DB");
+  },
+});
+
 //  This is required now
 yargs.parse();
+
+console.log(yargs.argv);
