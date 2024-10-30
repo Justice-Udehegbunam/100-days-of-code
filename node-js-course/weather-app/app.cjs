bbbce1cb7d2812a9169b30b29ededfbcec189cd6
Justice Udehegbunam <justicefavour491@gmail.com>
2024-10-30 07:04:26 +0100

    Added error handling functionality for v1 of weather app

--- node-js-course/weather-app/app.cjs
+++ node-js-course/weather-app/app.cjs
@@ -2,37 +2,47 @@
 
 const request = require("postman-request");
 
-const url =
-  "https://api.weatherstack.com/current?access_key=&query=6.8570,7.3834";
+// const url =
+//   "https://api.weatherstack.com/current?access_key=&query=6.8570,7.3834";
 
-// json - sets body to JSON representation of value and adds Content-type: application/json header. Additionally, parses the response body as JSON.
-request(url, { json: true }, (error, response) => {
-  // const data = JSON.parse(response.body);
+// // json - sets body to JSON representation of value and adds Content-type: application/json header. Additionally, parses the response body as JSON.
+// request(url, { json: true }, (error, response) => {
+//   // const data = JSON.parse(response.body);
 
-  const data = response.body.current;
+//   if (error) {
+//     console.log("No network detected");
+//   } else if (response.body.success === false) {
+//     console.log("Unable to find location!");
+//   } else {
+//     const data = response.body.current;
 
-  // console.log(
-  //   data.weather_descriptions[0] +
-  //     ` Its ${data.temperature} degrees, It feel's like ${data.feelslike} degrees out.`
-  // );
-});
+//     console.log(
+//       data.weather_descriptions[0] +
+//         ` Its ${data.temperature} degrees, It feel's like ${data.feelslike} degrees out.`
+//     );
+//   }
+// });
 
 // Geocoding api
 
 const geoAPI =
-  "https://api.opencagedata.com/geocode/v1/json?q=Nsukka&key=&limit=1";
+  "https://api.opencagedata.com/geocode/v1/json?q=missippi&key=&limit=1";
 
 request(geoAPI, { json: true }, (error, response) => {
-  const data = response.body.results[0];
+  if (error) {
+    console.log("Poor internet connectivity!");
+  } else if (response.body.status.code === 400) {
+    console.log("Please check your location input again!");
+  } else {
+    const data = response.body.results[0];
 
-  console.log(
-    "The geometry is " +
-      data.geometry.lat +
-      " by " +
-      data.geometry.lng +
-      " for " +
-      data.formatted
-  );
-
-  // console.log(data);
+    console.log(
+      "The geometry is " +
+        data.geometry.lat +
+        " by " +
+        data.geometry.lng +
+        " for " +
+        data.formatted
+    );
+  }
 });
