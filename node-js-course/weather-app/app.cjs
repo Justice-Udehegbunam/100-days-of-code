0211a4c643a661165596d70718cdff53208fad61
Justice Udehegbunam <justicefavour491@gmail.com>
2024-11-03 21:49:31 +0100

    Done with modularization of geocode API logic

--- node-js-course/weather-app/app.cjs
+++ node-js-course/weather-app/app.cjs
@@ -1,6 +1,7 @@
 /* Making HTTP Requests */
 
 const request = require("postman-request");
+const geoCode = require("./utils/geoCode.cjs");
 
 // const url = "https://api.weatherstack.com/current?access_key=53d5c6d178d53ebcb753860c72a59132&query=6.8570,7.3834";
 // // json - sets body to JSON representation of value and adds Content-type: application/json header. Additionally, parses the response body as JSON.
@@ -23,23 +24,28 @@ const request = require("postman-request");
 
 // Geocoding api
 
-const geoAPI =
-  "https://api.opencagedata.com/geocode/v1/json?q=ikki&key=bf04b86b881842e0898fd7b43bc4c529&limit=1";
-request(geoAPI, { json: true }, (error, response) => {
-  if (error) {
-    console.log("Poor internet connectivity!");
-  } else if (response.body.status.code === 400) {
-    console.log("Please check your location input again!");
-  } else {
-    const data = response.body.results[0];
+// const geoAPI =
+//   "https://api.opencagedata.com/geocode/v1/json?q=ikki&key=bf04b86b881842e0898fd7b43bc4c529&limit=1";
+// request(geoAPI, { json: true }, (error, response) => {
+//   if (error) {
+//     console.log("Poor internet connectivity!");
+//   } else if (response.body.status.code === 400) {
+//     console.log("Please check your location input again!");
+//   } else {
+//     const data = response.body.results[0];
 
-    console.log(
-      "The geometry is " +
-        data.geometry.lat +
-        " by " +
-        data.geometry.lng +
-        " for " +
-        data.formatted
-    );
-  }
+//     console.log(
+//       "The geometry is " +
+//         data.geometry.lat +
+//         " by " +
+//         data.geometry.lng +
+//         " for " +
+//         data.formatted
+//     );
+//   }
+// });
+
+geoCode("odim nsukka", (error, data) => {
+  console.log(error);
+  console.log(data);
 });
---
+++ node-js-course/weather-app/utils/geoCode.cjs
@@ -0,0 +1,26 @@
+const request = require("postman-request");
+
+const geoCode = (address, callback) => {
+  const url =
+    "https://api.opencagedata.com/geocode/v1/json?q=" +
+    encodeURIComponent(address) +
+    "&key=bf04b86b881842e0898fd7b43bc4c529&limit=1";
+
+  request(url, { json: true }, (error, response) => {
+    if (error) {
+      callback("Unable to connect to location services!");
+    } else if (response.body.status.code === 400) {
+      callback("Please check your location input and try again!");
+    } else {
+      const data = response.body.results[0];
+
+      callback(undefined, {
+        lat: data.geometry.lat,
+        long: data.geometry.lng,
+        location: data.formatted,
+      });
+    }
+  });
+};
+
+module.exports = geoCode;
