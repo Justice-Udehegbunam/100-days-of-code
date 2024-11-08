af7a013544cbc84a93fa569159f1762f8a174fe7
Justice Udehegbunam <justicefavour491@gmail.com>
2024-11-08 22:42:23 +0100

    Day 47 2nd commit

--- node-js-course/web-server/src/app.cjs
+++ node-js-course/web-server/src/app.cjs
@@ -6,6 +6,18 @@ app.get("", (req, res) => {
   res.send("Hello Express!!");
 });
 
+app.get("/help", (req, res) => {
+  res.send("Help page!");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About page!");
+});
+
+app.get("/weather", (req, res) => {
+  res.send("Weather page!");
+});
+
 app.listen(5000, () => {
   console.log("Server started on port 5000");
 });
