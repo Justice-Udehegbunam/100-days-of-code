2f58b920be114e8be9d7a2e2722c403cc28c0e41
Justice Udehegbunam <justicefavour491@gmail.com>
2024-12-04 20:56:48 +0100

    Todays progress

--- node-js-course/task-manager/mongodb.cjs
+++ node-js-course/task-manager/mongodb.cjs
@@ -1,10 +1,15 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 
 const connectURL = "mongodb://127.0.0.1:27017";
 
 const client = new MongoClient(connectURL);
 
 const dbName = "task-manager";
+
+const id = new ObjectId();
+console.log(id);
+
+console.log(id.getTimestamp());
 
 async function run() {
   try {
@@ -13,15 +18,34 @@ async function run() {
     console.log("Connected to the MongoDB server.");
 
     const db = client.db(dbName);
-    const result = await db
-      .collection("Users")
-      .insertOne({ name: "dotunmo", age: "12" }, (error, result) => {
+
+    // const taskCollection = await db.collection("Tasks");
+
+    // const tasks = [
+    //   { description: "Wash Plate", completed: true },
+    //   { description: "Fetch Water", completed: false },
+    //   { description: "Call mum", completed: true },
+    // ];
+
+    // taskCollection.insertMany(tasks, (error, result) => {
+    //   if (error) {
+    //     return console.log("Unable to insert tasks!");
+    //   }
+    // });
+    // //   console.log("Inserted ID:", result.insertedId);
+    // console.log("Insert successful!");
+
+    const result = await db.collection("Users").insertOne(
+      // the _id is only if you feel like it
+      { _id: id, name: "Tobi", age: 22 },
+
+      (error, result) => {
         if (error) {
-          return console.log("Unable to insert user!");
+          return console.log("Unable to insert docs!");
         }
-      });
-    console.log("Inserted ID:", result.insertedId);
-    console.log("Insert successful!");
+      }
+    );
+    console.log("Inserted ID:", result.insertedId); // returns the id of the inserted data
   } catch (error) {
     console.error("An error occurred:", error);
   } finally {
