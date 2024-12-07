724761e8cd2bd24f199f567af1b5f67dc1f5b911
Justice Udehegbunam <justicefavour491@gmail.com>
2024-12-07 17:07:12 +0100

    Commit for today

---
+++ node-js-course/task-manager/mongodb-js-progress/1-mongodb.cjs
@@ -0,0 +1,58 @@
+const { MongoClient, ObjectId } = require("mongodb");
+
+const connectURL = "mongodb://127.0.0.1:27017";
+
+const client = new MongoClient(connectURL);
+
+const dbName = "task-manager";
+
+const id = new ObjectId();
+console.log(id);
+
+console.log(id.getTimestamp());
+
+async function run() {
+  try {
+    await client.connect();
+
+    console.log("Connected to the MongoDB server.");
+
+    const db = client.db(dbName);
+
+    // const taskCollection = await db.collection("Tasks");
+
+    // const tasks = [
+    //   { description: "Wash Plate", completed: true },
+    //   { description: "Fetch Water", completed: false },
+    //   { description: "Call mum", completed: true },
+    // ];
+
+    // taskCollection.insertMany(tasks, (error, result) => {
+    //   if (error) {
+    //     return console.log("Unable to insert tasks!");
+    //   }
+    // });
+    // //   console.log("Inserted ID:", result.insertedId);
+    // console.log("Insert successful!");
+
+    const result = await db.collection("Users").insertOne(
+      // the _id is only if you feel like it
+      { _id: id, name: "Michael", age: 16 },
+
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert docs!");
+        }
+      }
+    );
+    console.log("Inserted ID:", result.insertedId); // returns the id of the inserted data
+  } catch (error) {
+    console.error("An error occurred:", error);
+  } finally {
+    // Ensure the client is closed
+    // await client.close();
+    // console.log("Connection to MongoDB server closed.");
+  }
+}
+
+run().catch(console.dir);
---
+++ node-js-course/task-manager/mongodb-js-progress/howPromisesWork.cjs
@@ -0,0 +1,21 @@
+//  the promise param names are important
+const doWorkPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // The order is really important as it can affect the state of what the promise will return first
+    resolve([7, 4, 1]);
+    reject("Something went wrong");
+  }, 2000);
+});
+
+doWorkPromise
+  .then((res) => {
+    console.log("success!", res);
+
+    /* returns  $ node howPromisesWork.cjs
+success! [ 7, 4, 1 ]
+*/
+  })
+  .catch((err) => {
+    console.log("Error: " + err);
+  });
+// the .then method allows us to get the value that comes through when a promise resolves successfully
