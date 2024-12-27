9dc0e2b901edc15a4c27b85e5c64789c3c3b855e
Justice Udehegbunam <justicefavour491@gmail.com>
2024-12-27 18:31:51 +0100

    Gotten started with first full-stack capstone project

---
+++ projects/midjourney-clone/client/src/App.jsx
@@ -0,0 +1,8 @@
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import { logo } from "./assets/index";
+
+const App = () => {
+  return <h1 className="text-3xl font-bold underline">Lets get started</h1>;
+};
+
+export default App;
